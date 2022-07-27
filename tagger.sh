39ac126ec79620bcca0421f05f762401cf16b4a8
BasicAcid <david.tabarie@gmail.com>
2022-07-27 11:21:07 +0200

    Add fzf support.

--- tagger.sh
+++ tagger.sh
@@ -12,9 +12,14 @@ The filenames should obey the following format:
 OPTIONS:
    -f Tag1 Tag2 TagN Return matching files
    -p Tag1 Tag2 TagN Return matching files with the path
+   -x Tag1 Tag2 TagN Same as -p but pass the result to fzf
    -l                List all existing tags
    -a                List all tagged files
    -n                List all files not tagged
+
+Examples:
+  tagger.sh -f papers
+  tagger.sh -x "papers CS"
 EOF
 }
 
@@ -84,11 +89,26 @@ find_by_tags_fullpath () {
     eval "$final_command"
 }
 
-while getopts ":hf:p:aln" OPTION; do
+xdg_open_from_fzf () {
+    for var in $@
+    do
+        tag_to_grep="| grep --color=auto '\\/(.*${var}.*)'"
+        str_to_grep="${str_to_grep} ${tag_to_grep}"
+    done
+
+    cmd="find . -name '*' -type f -print"
+
+    final_command="$cmd $str_to_grep"
+
+    eval "$final_command | fzf | xargs -I {} echo '\"{}\"' | xargs xdg-open"
+}
+
+while getopts ":hf:p:x:aln" OPTION; do
       case $OPTION in
           h) usage exit 1 ;;
           f) find_by_tags "${OPTARG}";;
           p) find_by_tags_fullpath "${OPTARG}";;
+          x) xdg_open_from_fzf "${OPTARG}";;
           l) list_tags;;
           a) list_tagged_files;;
           n) list_not_tagged_fullpath;;
